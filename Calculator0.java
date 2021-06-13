7db44edc9f74f0212554cc8f12cc2eebd70d2c68
jeanbacha <85833882+jeanbacha@users.noreply.github.com>
2021-06-13 17:00:54 +0300

    Add files via upload

---
+++ Calculator0.java
@@ -0,0 +1,22 @@
+package com.simplilearn.lvc1;
+
+import java.lang.Math;
+
+public class Calculator0 {
+		static int a=7;
+		static int b=4;
+
+	public static void main (String[] args) {
+		int ad=a+b;
+		int su=a-b;
+		int mu=a*b;
+		float di=a/b;
+		double sq = Math.sqrt(a);
+		System.out.println("a=7 and b=4; a+b = "+ ad);
+		System.out.println("a=7 and b=4; a-b = "+ su);
+		System.out.println("a=7 and b=4; a*b = "+ mu);
+		System.out.println("a=7 and b=4; a/b = "+ di);
+		System.out.println("a=7 and b=4; SQRT(a) = "+ sq);
+	}
+		
+}
